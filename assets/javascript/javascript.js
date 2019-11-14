39f96598f75a0e27beed983e67aa2382c33d77a8
Taylor Zettel <tzettel@gmail.com>
2019-11-13 19:33:48 -0500

    created searchGiphy function to test out properties of the Giphy API

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // declares global variables 
+
+  var searchGiphy = function(data) {
+    var apiKey = 'pu7fI2DSRxDuoVZVLEC2wTTlIsmfmdmF';
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?limit=1&api_key=' + apiKey + '&q=' + data;
+    $.ajax({
+      url: queryURL,
+      method: 'get'
+    })
+    .then(function(response) {
+      console.log(response);
+      var image = $('<img>');
+      image.attr('src', response.data[0].images.original_still.url);
+      $('#image').append(image);
+      console.log(image.html());
+    });
+  }
+  searchGiphy('dog');
+});
