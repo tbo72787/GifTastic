91faefd639455e94498172f2ad290e85c98ca9ec
Taylor Zettel <tzettel@gmail.com>
2019-11-13 20:20:05 -0500

    created page layout(including instrument addition form), created instrument array

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-  // declares global variables 
+  // declares global variables
+  var instArr = ['guitar', 'piano', 'saxaphone', 'trumpet', 'drum', 'tambourine', 'violin'] 
 
   var searchGiphy = function(data) {
     var apiKey = 'pu7fI2DSRxDuoVZVLEC2wTTlIsmfmdmF';
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?limit=1&api_key=' + apiKey + '&q=' + data;
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?limit=10&api_key=' + apiKey + '&q=' + data;
     $.ajax({
       url: queryURL,
       method: 'get'
