5f473aed4d3c56caeaea55a7f5292dc4a827ac1c
Taylor Zettel <tzettel@gmail.com>
2019-11-16 05:44:33 -0500

    added buttonPop function to populate buttons on screen for each item of instArr

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,20 +2,31 @@ $(document).ready(function() {
   // declares global variables
   var instArr = ['guitar', 'piano', 'saxaphone', 'trumpet', 'drum', 'tambourine', 'violin'] 
 
-  var searchGiphy = function(data) {
+  var buttonPop = function() {
+    for (var i = 0; i < instArr.length; i++) {
+      var button = $('<button>');
+      button.addClass('button');
+      button.text(instArr[i]);
+      $('#buttons').append(button);
+    }
+  }
+
+  var searchGiphy = function(term) {
     var apiKey = 'pu7fI2DSRxDuoVZVLEC2wTTlIsmfmdmF';
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?limit=10&api_key=' + apiKey + '&q=' + data;
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?limit=10&api_key=' + apiKey + '&q=' + term;
     $.ajax({
       url: queryURL,
       method: 'get'
     })
     .then(function(response) {
       console.log(response);
-      var image = $('<img>');
-      image.attr('src', response.data[0].images.original_still.url);
-      $('#image').append(image);
-      console.log(image.html());
+      for (var i = 0; i < response.data.length; i++) {
+        var image = $('<img>');
+        image.attr('src', response.data[i].images.fixed_height_still.url);
+        $('#images').append(image);
+      }
     });
+    
   }
-  searchGiphy('dog');
+  buttonPop();
 });
