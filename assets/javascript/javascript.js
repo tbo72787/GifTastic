1b5b28c4a647f2d908b01a77db7ab923e413e4c6
Taylor Zettel <tzettel@gmail.com>
2019-11-16 06:55:43 -0500

    added onclick event to dynamically created buttons to call searchGiphy function, made searchGiphy function empty images div before populating new images

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   // declares global variables
-  var instArr = ['guitar', 'piano', 'saxaphone', 'trumpet', 'drum', 'tambourine', 'violin'] 
+  var instArr = ['guitar', 'piano', 'saxaphone', 'trumpet', 'drum', 'tambourine', 'violin'];
 
+  // empties buttons div, takes each string in instArr and appends a button for it in buttons div
   var buttonPop = function() {
+    $('#buttons').empty();
     for (var i = 0; i < instArr.length; i++) {
       var button = $('<button>');
       button.addClass('button');
@@ -10,15 +12,21 @@ $(document).ready(function() {
       $('#buttons').append(button);
     }
   }
-
+  // when submit button is clicked, adds the contents of the input field to instArr and calls buttonPop
   $('#submit-button').on('click', function(event) {
-    instArr = [];
     event.preventDefault();
     instArr.push($('#submit-input').val().trim());
     buttonPop();
-  })
+  });
 
+  $('body').on('click', '.button', function() {
+    console.log($(this).text());
+    searchGiphy($(this).text());
+  });
+
+  // makes a call to giphy API and makes and appends an image for each GIF in the response
   var searchGiphy = function(term) {
+
     var apiKey = 'pu7fI2DSRxDuoVZVLEC2wTTlIsmfmdmF';
     var queryURL = 'https://api.giphy.com/v1/gifs/search?limit=10&api_key=' + apiKey + '&q=' + term;
     $.ajax({
@@ -26,6 +34,7 @@ $(document).ready(function() {
       method: 'get'
     })
     .then(function(response) {
+      $('#images').empty();
       console.log(response);
       for (var i = 0; i < response.data.length; i++) {
         var image = $('<img>');
